dd28ed9196a219472ce6146829a329cf4ed3c37d
Matheus Hernandes <midia.matheus@gmail.com>
2017-02-12 21:28:57 -0200

    fix gulpfile paths

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ var gulp = require('gulp'),
 var base = '',
 	devBase = base + 'src',
 	paths = {
-		dev: './src/shield.scss',
-		watch_scss: './src/*.scss',
+		dev: './dev/main.scss',
+		watch_scss: ['./dev/*.scss', './dev/**/*.scss', './dev/**/**/*.scss'],
 		watch_html: './*.html',
 		out: './assets'
 };
@@ -27,7 +27,8 @@ gulp.task('rebuild', function() {
 
 gulp.task('styles', function() {
     gulp.src(paths.dev)
-        .pipe(scss().on('error', scss.logError))
+    	.pipe(plumber())
+        .pipe(scss({outputStyle: 'compressed'}).on('error', scss.logError))
         .pipe(rename('main.css'))
         .pipe(gulp.dest(paths.out));
 });
