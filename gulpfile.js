1008c01270336ba13f78545d2b20fa300b2546c6
Matheus Hernandes <midia.matheus@gmail.com>
2017-02-12 22:48:03 -0200

    add basic layout files and styles

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var base = '',
 	devBase = base + 'src',
 	paths = {
 		dev: './dev/main.scss',
-		watch_scss: ['./dev/*.scss', './dev/**/*.scss', './dev/**/**/*.scss'],
+		watch_scss: ['./src/*.scss', './dev/*.scss', './dev/**/*.scss', './dev/**/**/*.scss'],
 		watch_html: './*.html',
 		out: './assets'
 };
@@ -30,11 +30,12 @@ gulp.task('styles', function() {
     	.pipe(plumber())
         .pipe(scss({outputStyle: 'compressed'}).on('error', scss.logError))
         .pipe(rename('main.css'))
-        .pipe(gulp.dest(paths.out));
+        .pipe(gulp.dest(paths.out))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 //Watch task
 gulp.task('default', ['styles', 'build'], function() {
-		gulp.watch(paths.watch_scss, ['style']);
+		gulp.watch(paths.watch_scss, ['styles']);
 		gulp.watch(paths.watch_html, ['rebuild']);
 });
